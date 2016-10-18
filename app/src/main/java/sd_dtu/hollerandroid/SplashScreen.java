3bb5930e28b8566be54d49ebb4429e1bd2592376
Krishna Rai <krishnarai30@gmail.com>
2016-10-19 01:00:06 +0530

    Splash Screen Done

--- app/src/main/java/sd_dtu/hollerandroid/SplashScreen.java
+++ app/src/main/java/sd_dtu/hollerandroid/SplashScreen.java
@@ -1,13 +1,30 @@
 package sd_dtu.hollerandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.splash_screen);
+        Thread myThread=new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
     }
 }
