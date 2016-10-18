73cd77778add0070b1c43e1038aa78ae96024894
Krishna Rai <krishnarai30@gmail.com>
2016-10-18 22:42:22 +0530

    Splash Screen

--- app/src/main/java/sd_dtu/hollerandroid/SplashScreen.java
+++ app/src/main/java/sd_dtu/hollerandroid/SplashScreen.java
@@ -3,11 +3,11 @@ package sd_dtu.hollerandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
     }
 }
